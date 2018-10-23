633893a9f3b68e93675c0f852d0350e6eedfe582
hantarr01 <thunderbolt655@mail.ru>
2018-10-23 10:00:27 +0300

    Work v.0.2.5.

---
+++ script.js
@@ -0,0 +1,14 @@
+(function($) {
+
+ $(document).ready(function(){
+    $('.slider').bxSlider({
+     auto:true,
+     autoControls:true,
+     stopAutoClick:true,
+     pager:true,
+     slideWidth:300
+     });
+  });
+
+ })(jQuery);
+
